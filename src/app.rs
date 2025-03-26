aab5a7838c853c70568b14da2e4089e4a9a95c2a
Jeff Palmucci <jeff@causallabs.io>
2025-03-26 18:04:12 -0400

    serve beep locally

--- src/app.rs
+++ src/app.rs
@@ -596,7 +596,7 @@ pub fn beep() {
     use js_sys::eval;
     let result = eval(&format!(
         "
-    new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3').play();
+    new Audio('/beep.mp3').play();
     ",
     ));
     if let Err(e) = result {
